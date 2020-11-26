6242dd53e94b991904213b642cc100868ddbfd2a
shahi <shahidfarid4577@gmail.com>
2020-11-26 10:41:02 +0500

    import a awesome fonts

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,6 +20,9 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child:RepeatContainerCode(
                   colors: Color(0xFF1D1E33),
+                  cardWidget: Column(
+
+                  ),
 
                 ),
               ),
@@ -45,8 +49,9 @@ class _InputPageState extends State<InputPage> {
 }
 
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
+  RepeatContainerCode({@required this.colors, this.cardWidget});
 final Color colors;
+final Widget cardWidget;
   @override
   Widget build(BuildContext context) {
     return Container(
